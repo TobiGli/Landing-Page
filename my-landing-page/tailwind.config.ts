eda428a1e287900c4a702af2c41d3533c595c3ff
TobiGLi <tobiasgomez99.tg@gmail.com>
2025-03-31 19:43:36 -0300

    Actualizacion y maquetado

--- my-landing-page/tailwind.config.ts
+++ my-landing-page/tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 export default {
+  
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{css,js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
